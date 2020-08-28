960bcf1c4ca5e8d92b062a7ebeb965d200fe1d35
Marnus Coetzee <marnuscoe@gmail.com>
2020-08-28 17:14:42 +0200

    changed auth page ui

--- src/app/artist/pages/artist-auth/artist-auth.component.ts
+++ src/app/artist/pages/artist-auth/artist-auth.component.ts
@@ -1,10 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { Subscription } from 'rxjs';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { Router } from '@angular/router';
-import { AngularFireAuth } from '@angular/fire/auth';
-import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-artist-auth',
@@ -13,105 +8,53 @@ import * as firebase from 'firebase';
 })
 export class ArtistAuthComponent implements OnInit {
 
-  authSubscription: Subscription;
+  hide = true;
 
-  isLoading = false;
+  loginForm: FormGroup;
+  signupForm: FormGroup;
 
-  form: FormGroup;
+  isLoading: boolean;
 
-  type: 'login' | 'signup' | 'reset' = 'signup';
-
-  serverMessage: string;
+  showLogin = true;
+  showSignup = false;
+  showForgotPassword = false;
 
   constructor(
-    private fb: FormBuilder,
-    private afAuth: AngularFireAuth,
-    private router: Router,
-    private db: AngularFirestore
+    private fb: FormBuilder
   ) { }
 
   ngOnInit(): void {
-    this.form = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.minLength(6), Validators.required]],
-      passwordConfirm: ['', []]
+    this.initLoginForm();
+    this.initSignupForm();
+  }
+
+  private initLoginForm() {
+    this.loginForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
     });
   }
 
-  changeType(val) {
-    this.type = val;
+  private initSignupForm() {
+    this.signupForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      userName: ['', Validators.required],
+      pass1: ['', [Validators.required, Validators.minLength(6)]],
+      pass2: ['', [Validators.required, Validators.minLength(6)]]
+    });
   }
 
-  get isLogin() {
-    return this.type === 'login';
+  onClickShowSignup() {
+    this.showSignup = true;
+    this.showLogin = false;
+    this.showForgotPassword = false;
   }
 
-  get isSignup() {
-    return this.type === 'signup';
+  onClickShowLogin() {
+    this.showSignup = false;
+    this.showLogin = true;
+    this.showForgotPassword = false;
   }
-
-  get isPasswordReset() {
-    return this.type === 'reset';
-  }
-
-  get email() {
-    return this.form.get('email');
-  }
-  get password() {
-    return this.form.get('password');
-  }
-
-  get passwordConfirm() {
-    return this.form.get('passwordConfirm');
-  }
-
-  get passwordDoesMatch() {
-    if (this.type !== 'signup') {
-      return true;
-    } else {
-      return this.password.value === this.passwordConfirm.value;
-    }
-  }
-
-      // tslint:disable-next-line: typedef
-  async onSubmit() {
-    this.isLoading = true;
-
-    const email = this.email.value;
-    const password = this.password.value;
-
-    try {
-      if (this.isLogin) {
-        await this.afAuth.setPersistence(firebase.auth.Auth.Persistence.LOCAL)
-        .then(() => this.afAuth.signInWithEmailAndPassword(email, password))
-        .then(() => this.router.navigate(['artist/dashboard']));
-      }
-      if (this.isSignup) {
-        await this.afAuth.setPersistence(firebase.auth.Auth.Persistence.LOCAL)
-        .then(() => this.afAuth.createUserWithEmailAndPassword(email, password))
-        .then((data) => {
-          const uid = data.user.uid;
-          this.db.collection('users').doc(uid).set({uid, email});
-        })
-        .then(() => this.router.navigate(['artist/create-profile']))
-        .catch((err) => console.log(err));
-      }
-      if (this.isPasswordReset) {
-        await this.afAuth.sendPasswordResetEmail(email);
-        this.serverMessage = 'Check Your Email';
-      }
-    } catch (err) {
-      this.serverMessage = err;
-    }
-
-    this.isLoading = false;
-  }
-
-  ngOnDestroy(): void {
-    if (this.authSubscription) {
-      this.authSubscription.unsubscribe();
-    }
-  }
-  
 
 }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD4ZnbTGjXL_ps4YhyY1MklZBxlz2vhV7E",
+    authDomain: "online-shop-435be.firebaseapp.com",
+    databaseURL: "https://online-shop-435be.firebaseio.com",
+    projectId: "online-shop-435be",
+    storageBucket: "online-shop-435be.appspot.com",
+    messagingSenderId: "912600155906",
+    appId: "1:912600155906:web:ed7968a3f937a1a54c241b",
+    measurementId: "G-T33HL8XM93"
+  },
+  adminId: 'zcqzrIz7VOcqfLzQvxDgZczpnL72'
+};
+
+// firebase deploy --only hosting:pixr-client
